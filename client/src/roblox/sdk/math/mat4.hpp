a6a567bce6c140444f205075690b83855feb6c73
umbre11a <4idorique@gmail.com>
2026-04-25 21:05:33 -0700

    add vec4 , math4

---
+++ client/src/roblox/sdk/math/mat4.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+
+namespace 
+	geom{ 
+
+template< typename T > 
+struct mat4_tpl{
+	T data[ 16 ];
+};
+	
+	typedef mat4_tpl< float >			mat4;
+} // geom
---
+++ client/src/roblox/sdk/math/vec4.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+
+namespace
+	geom{
+
+	template< typename T >
+	struct vec4_tpl{
+			T x , y , z , w;		
+
+		vec4_tpl( ) noexcept : x( 0 ) , y( 0 ) , z( 0 ) , w( 0 ){ }
+		vec4_tpl( const vec4_tpl& t ) noexcept : x( t.x ) , y( t.y ) , z( t.z ) , w( t.w ){ }
+		vec4_tpl( T a , T b , T c , T d ) noexcept : x( a ) , y( b ) , z( c ) , w( d){ }
+	
+		void set( T a , T b , T c , T d ) noexcept { x = a; y = b; z = c; w = d; }
+
+		T get_length( ) const noexcept { 
+			return ( T )sqrt( x * x + y * y + z * z + w * w ); 
+		}
+
+		T get_distance( const vec4_tpl& b ) const noexcept { 
+			return ( T )sqrt( 
+				( x - b.x ) * ( x - b.x) +
+				( y - b.y )	* ( y - b.y ) +
+				( z - b.z ) * ( z - b.z ) +
+				( w - b.w ) * ( w - b.w )
+			);
+		}
+
+		vec4_tpl operator +( const vec4_tpl& a ){
+			return vec4_tpl( x + a.x , y + a.y , z + a.z , w + a.w );
+		}
+		vec4_tpl operator -( const vec4_tpl& a ){
+			return vec4_tpl( x - a.x , y - a.y , z - a.z , w - a.w );
+		}
+
+		bool is_zero( ) const noexcept { return x == 0 && y == 0 && z == 0; w == 0}
+	};
+
+	typedef vec4_tpl< float >		vec4;
+} // geom
+
+
